0358d753ddc7035ae541a5fe8c184658abed73be
tangi <tangi@UBUNTU.myguest.virtualbox.org>
2024-02-24 11:07:42 +0100

    reecriture de init plateau pour utiliser les enum. ajout de la fonction affiche Case

--- php/functions_plateau.php
+++ php/functions_plateau.php
@@ -3,28 +3,56 @@
 /**
  * initialise le plateau de jeu pour le début de la partie.
  */
+
+use function PHPSTORM_META\type;
+
+ /**
+  * enumeration des directions possibles pour les pions
+  */
 enum Direction{
     case Haut;
     case Bas;
     case Gauche;
     case Droite;
+    case Neutre;//neutre est la direction utilisée pour les cases Montagne et Vide
 }
 
-enum Pion{
+/**
+ * enumeration des différents types de cases
+ */
+enum typeCase{
     case Montagne;
     case Elephant;
     case Rhinoceros;
+    case Vide;
 }
+
+/**
+ * initialise le plateau pour le début de partie
+ */
  function initPlateau(){
     $plateau=array();
     for($i=0;$i<5;$i++){
-        $plateau[]=array("","","","","");
+        $plateau[]=array(array(typeCase::Vide,Direction::Neutre),
+        array(typeCase::Vide,Direction::Neutre),
+        array(typeCase::Vide,Direction::Neutre),
+        array(typeCase::Vide,Direction::Neutre),
+        array(typeCase::Vide,Direction::Neutre));
     }
-    $plateau[]=array("eh","eh","eh","eh","eh");
-    $plateau[]=array("rh","rh","rh","rh","rh");
-    $plateau[2][1]="M";
-    $plateau[2][2]="M";
-    $plateau[2][3]="M";
+    $plateau[]=array(array(typeCase::Elephant,Direction::Haut),
+    array(typeCase::Elephant,Direction::Haut),
+    array(typeCase::Elephant,Direction::Haut),
+    array(typeCase::Elephant,Direction::Haut),
+    array(typeCase::Elephant,Direction::Haut));
+    $plateau[]=array(array(typeCase::Rhinoceros,Direction::Haut),
+    array(typeCase::Rhinoceros,Direction::Haut),
+    array(typeCase::Rhinoceros,Direction::Haut),
+    array(typeCase::Rhinoceros,Direction::Haut),
+    array(typeCase::Rhinoceros,Direction::Haut));
+    $plateau[]=array(typeCase::Vide,Direction::Neutre);
+    $plateau[2][1]=array(typeCase::Montagne,Direction::Neutre);
+    $plateau[2][2]=array(typeCase::Montagne,Direction::Neutre);
+    $plateau[2][3]=array(typeCase::Montagne,Direction::Neutre);
 
     return $plateau;
 }
@@ -33,14 +61,59 @@ enum Pion{
  * fonction de debug pour afficher le plateau en texte.
  */
 function debugAffichePlateau($plateau){
-    foreach($plateau as $i => $ligne){
-        foreach($ligne as $j => $case){
-            echo "|".$case."|";
+    foreach($plateau as $ligne){
+        foreach($ligne as $case){
+            echo "|".afficheCase($case)."|";
         }
         echo "<br>";
     }
 }
 
+/**
+ * affiche une case du plateau (un tableau [typeCase, Direction])
+ */
+function afficheCase($case){
+    $type=$case[0];
+    $direction=$case[1];
+    switch($type){
+        case typeCase::Rhinoceros:
+            $strType="R";
+            break;
+        case typeCase::Elephant:
+            $strType= "E";
+            break;
+        case typeCase::Montagne:
+            $strType= "M";
+            break;
+        case typeCase::Vide:
+            $strType= "V";
+            break;
+        default:
+            $strType= "Err";
+            break;
+        }
+    switch($direction){
+        case Direction::Haut:
+            $strDirection="H";
+            break;
+        case Direction::Bas:
+            $strDirection= "B";
+            break;
+        case Direction::Gauche:
+            $strDirection= "G";
+            break;
+        case Direction::Droite:
+            $strDirection= "D";
+            break;
+        case Direction::Neutre:
+            $strDirection= "N";
+            break;
+        default:
+            $strDirection= "Err";
+            break;
+        }
+        return $strDirection." ".$strType;
+}
 /**
  * encode le plateau en JSON
  */
